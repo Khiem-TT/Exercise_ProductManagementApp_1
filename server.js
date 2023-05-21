cd507a8afbcace2b46b699fc4f1066844c67bf07
tranthekhiem14112001 <trankhiem99999@gmail.com>
2023-05-21 22:55:37 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,29 @@
+let http = require('http');
+let fs = require('fs');
+
+let server = http.createServer((req, res) => {
+    let dataFile = '';
+    let html = '';
+    fs.readFile('./products.json', 'utf-8', (err, data) => {
+        dataFile = JSON.parse(data);
+        dataFile.forEach((value, index) => {
+            html += '<tr>';
+            html += `<td>${index + 1}</td>`;
+            html += `<td>${value.name}</td>`;
+            html += `<td>${value.price}</td>`;
+            html += `<td><button>Delete</button></td>`;
+            html += `<td><button>Update</button></td>`;
+            html += '</tr>';
+        });
+    });
+    fs.readFile('./listproducts.html', 'utf-8', (err, data) => {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        data = data.replace('{list-products}', html);
+        res.write(data);
+        res.end();
+    });
+});
+
+server.listen(8000, () => {
+    console.log('http://localhost:8000');
+});
